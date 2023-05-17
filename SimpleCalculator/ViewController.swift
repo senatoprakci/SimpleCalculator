4bd08b596cd6f223ad76fe00c2ba8311754780bd
Sena Toprakcı <74680647+senatoprakci@users.noreply.github.com>
2023-05-17 11:51:06 +0300

    Initial Commit

---
+++ SimpleCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleCalculator
+//
+//  Created by Sena Toprakcı on 17.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
